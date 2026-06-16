bafbaa9eee799e5cd9b0e83bc730eceb6ea423e8
Kaio Silva <kaiocastlho900@gmai.com>
2026-06-16 20:58:40 -0300

    Adiciona projeto INTERFACE_POSTRATA

---
+++ INTERFACE_POSTRATA/INTERFACE_POSTRATA/CadastroPaciente.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INTERFACE_POSTRATA
+{
+    /// <summary>
+    /// Lógica interna para CadastroPaciente.xaml
+    /// </summary>
+    public partial class CadastroPaciente : Window
+    {
+        public CadastroPaciente()
+        {
+            InitializeComponent();
+        }
+        private void VoltarMenu_Click(object sender, RoutedEventArgs e)
+        {
+            new Window1().Show();
+            this.Close();
+        }
+
+    }
+}
